04b69980fe0787aa1cea23071af056b9a5b51712
Atakan Atay <atakan.atay106@gmail.com>
2024-07-15 21:14:55 +0300

    Last.version

--- dicegame.js
+++ dicegame.js
@@ -16,6 +16,7 @@ function rollDie() {
 
 function showRoll() {
   if (gameEnded) return;
+
   const result1Element = document.getElementById("roll1");
   const result2Element = document.getElementById("roll2");
   const player1RollsElement = document.getElementById("total-1");
@@ -23,64 +24,70 @@ function showRoll() {
   const turnElement = document.getElementById("turn");
   const gameResultElement = document.getElementById("result");
 
-  const roll = rollDie(); // Her atış için yeni bir zar atışı yap
-  if (total1 < 29 || total2 < 29) {
-    if (currentPlayer === 1) {
-      if (roll != 1) {
-        total1 += roll;
-        result1Element.textContent = total1;
-        player1RollsElement.textContent = roll;
-        currentPlayer = 2;
-        turnElement.textContent = "Player 2's Turn"; // Sırayı bir sonraki oyuncuya geçir
+  let roll = rollDie(); // İlk zar atışı
 
-        player2El.style.backgroundColor = "red"; // Player 2 kutucuğunun rengini kırmızı yap
-
-        player1El.style.backgroundColor = "#ffabff";
-      } else  {
-        total1 = 0;
-        result1Element.textContent = "0";
+  if (currentPlayer === 1) {
+    if (roll === 1) {
+      total1 = 0;
+      result1Element.textContent = "0";
+      currentPlayer = 2;
+      turnElement.textContent = "Player 2's Turn";
+      player1RollsElement.textContent = roll;
+      player2El.style.backgroundColor = "red";
+      player1El.style.backgroundColor = "#ffabff";
+    } else {
+      total1 += roll;
+      result1Element.textContent = total1;
+      player1RollsElement.textContent = roll;
+      if (roll !== 6) {
         currentPlayer = 2;
         turnElement.textContent = "Player 2's Turn";
-        document.body.style.backgroundColor = "red";
-        player1RollsElement.textContent = roll;
-        player2El.style.backgroundColor = "red"; // Player 2 kutucuğunun rengini kırmızı yap
-
+        player2El.style.backgroundColor = "red";
         player1El.style.backgroundColor = "#ffabff";
+      } else {
+        turnElement.textContent = "Player 1's Turn (Roll again)";
+        player1El.style.backgroundColor = "red";
+        player2El.style.backgroundColor = "#ffabff";
       }
-    } else if (currentPlayer === 2) {
-      if (roll != 1) {
-        total2 += roll;
-        result2Element.textContent = total2;
-        player2RollsElement.textContent = roll;
-        currentPlayer = 1; // Sırayı bir sonraki oyuncuya geçir
+    }
+  } else if (currentPlayer === 2) {
+    if (roll === 1) {
+      total2 = 0;
+      result2Element.textContent = "0";
+      currentPlayer = 1;
+      turnElement.textContent = "Player 1's Turn";
+      player2RollsElement.textContent = roll;
+      player1El.style.backgroundColor = "red";
+      player2El.style.backgroundColor = "#ffabff";
+    } else {
+      total2 += roll;
+      result2Element.textContent = total2;
+      player2RollsElement.textContent = roll;
+      if (roll !== 6) {
+        currentPlayer = 1;
         turnElement.textContent = "Player 1's Turn";
-        player1El.style.backgroundColor = "red"; // Player 1 kutucuğunun rengini eski haline döndür
-
+        player1El.style.backgroundColor = "red";
         player2El.style.backgroundColor = "#ffabff";
       } else {
-        total2 = 0;
-        result2Element.textContent = "0";
-        currentPlayer = 1;
-        player2RollsElement.textContent = roll;
-        turnElement.textContent = "Player 1's Turn";
-        player1El.style.backgroundColor = "red"; // Player 1 kutucuğunun rengini eski haline döndür
-
-        player2El.style.backgroundColor = "#ffabff";
+        turnElement.textContent = "Player 2's Turn (Roll again)";
+        player2El.style.backgroundColor = "red";
+        player1El.style.backgroundColor = "#ffabff";
       }
     }
-    if (total1 > 29 && total1 > total2) {
-      gameResultElement.textContent = "Player 1 Wins!!!";
-      gameEnded = true;
-      setTimeout(() => {
-        alert("Game over, start a new game.");
-      }, 3000);
-    } else if (total2 > 29 && total2 > total1) {
-      gameResultElement.textContent = "Player 2 Wins!!!";
-      gameEnded = true;
-      setTimeout(() => {
-        alert("Game over, start a new game.");
-      }, 1000);
-    }
+  }
+
+  if (total1 >= 30 && total1 > total2) {
+    gameResultElement.textContent = "Player 1 Wins!!!";
+    gameEnded = true;
+    setTimeout(() => {
+      alert("Game over, start a new game.");
+    }, 3000);
+  } else if (total2 >= 30 && total2 > total1) {
+    gameResultElement.textContent = "Player 2 Wins!!!";
+    gameEnded = true;
+    setTimeout(() => {
+      alert("Game over, start a new game.");
+    }, 1000);
   }
 
   switch (roll) {
@@ -103,14 +110,14 @@ function showRoll() {
       diceEl.src = "dice-6.png";
       break;
     default:
-      diceEl.src = "dice-unknown.png"; // Varsayılan olarak bilinmeyen zar görseli
+      diceEl.src = "dice-1.png";
   }
 
-  // Zar resmini hareket ettirme animasyonu eklemek için
+  // animasyon
   diceEl.classList.add("dice-roll-animation");
   setTimeout(() => {
     diceEl.classList.remove("dice-roll-animation");
-  }, 300); // 0. saniye sonra animasyonu kaldır
+  }, 300);
 }
 
 function resetGame() {
@@ -134,11 +141,10 @@ function resetGame() {
   gameResultElement.textContent = "";
 
   player1El.style.backgroundColor = "#ffabff";
-
   player2El.style.backgroundColor = "#ffabff";
   gameEnded = false;
   currentPlayer = 1; // Oyuncu sırasını sıfırla
-  turnElement.textContent = "Player 2's Turn"; // Oyun sıfırlandığında Player 1'ın sırası
+  turnElement.textContent = "Player 1's Turn"; // Oyun sıfırlandığında Player 1'ın sırası
 }
 
 resetButton.addEventListener("click", resetGame);
